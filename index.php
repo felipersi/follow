a0c35d961dcc86148d57aa9c341258b2b86d3dc8
Bruno Minossi <b.minossi@gmail.com>
2019-09-23 20:56:57 -0300

    Removendo variáveis de teste da index

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 include_once('model/classAPI.php');
 
-//$idchamado = '2516475';
 $requisicaoapi = new api();
 $phpsessid = $requisicaoapi->loga("bruno.minossi","218211415kinghost");
-//var_dump($phpsessid);
 
 $idBko = $requisicaoapi-> numResponsavel($phpsessid);
 var_dump($requisicaoapi-> consultaBko($phpsessid,1,$idBko));
