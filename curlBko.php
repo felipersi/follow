285806305930dda0b5ed26df06ceab7932f52e27
felipersi <luis.felipersi@gmail.com>
2019-09-23 16:52:24 -0300

    atualização OO do curlbkp e classapi

--- curlBko.php
+++ curlBko.php
@@ -1,21 +1,9 @@
 <?php
-function loga($nome,$senha,$finger){
-$ch= curl_init();
-echo $senha;
-curl_setopt($ch,CURLOPT_URL, "http://backoffice.kinghost.net/login");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_COOKIEJAR, "./cookies/$nome.txt");
-curl_setopt($ch, CURLOPT_POSTFIELDS, "login=$nome&senha=$senha&submit=Entrar");
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-$interacoes= curl_exec($ch);
-curl_close($ch);
-if(substr_count($interacoes,"Dados Incorretos") == 0){
-        shell_exec("echo 'FINGERPRINTID '$finger >> ./cookies/$nome.txt");
-        echo "1";
-}
-else{
-echo "0";
-shell_exec("rm -f ./cookies/$nome.txt");
-}
-}
+include_once('model/classAPI.php');
 
+$idchamado = '207329';
+$nome='luis.silva';
+$senha='migra201125';
+$finger='';
+$requisicaoapi = new api($nome, $senha, $finger);
+$requisicaoapi->loga($idchamado);
--- model/classAPI.php
+++ model/classAPI.php
@@ -1,22 +1,46 @@
 <?php
 class api
 {
-    function consulta ($idchamado)
+	private $url;
+	private $usuario;
+	private $senha;
+
+	function _construct($usuario,$senha){
+
+		$this->url = 'http://backoffice.kinghost.net/login';
+		$this->usuario = $usuario;
+		$this->senha = $senha;
+
+	}
+   public function consulta ($idchamado)
     {
 		$handle = curl_init();
  
 		$url = "http://ws-backoffice.kinghost.net/chamados/" . $idchamado . "/interacoes";
  
-		curl_setopt($handle, CURLOPT_URL, $url);
+		curl_setopt($handle, CURLOPT_URL, $this->url);
 		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
   		curl_setopt($handle, CURLOPT_HTTPHEADER, array( 'Accept: application/json')); 
 		$output = curl_exec($handle);
 		$output = json_decode($output);
  		echo "<pre>";
  		var_dump($output->count);
- 		echo "</pre>";
- 		
-
-
+ 		echo "</pre>";	
     }
+
+    public function loga ($usuario,$senha){
+	
+	$this->usuario;
+	$this->senha;
+	
+	}
+
+	public function numResponsavel(){
+		$idBko = shell_exec("curl -s -H 'Cookie: PHPSESSID=".$phpsessid."' 'http://www.backoffice.kinghost.net/bug' | egrep -o 'login.*$'");
+		$idBko = preg_replace( '/[^0-9]/', '', $idBko);
+
+	}
+}
+
+
 }
