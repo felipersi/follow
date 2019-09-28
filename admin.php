fadcdfd5926d4358d02831e34b6bbf7005fc65ff
Bruno Minossi <b.minossi@gmail.com>
2019-09-28 03:14:15 -0300

    Separei a index em arquivo index de login e admin pra gerar o sistema. Sessão de php integrada e pode ser destruida com ?sair, caindo em um if do get do admin.php

---
+++ admin.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+include_once('model/classAPI.php');
+if(isset($_GET['sair'])){
+	unset($_SESSION["usuario"]);
+	header("Location: index.php");
+}
+if(isset($_SESSION["usuario"])&&isset($_SESSION["senha"])){
+	$usuario = $_SESSION["usuario"];
+	$senha = $_SESSION["senha"];
+
+	$requisicaoapi = new api($usuario, $senha);
+	$requisicaoapi -> loga();
+	$phpsessid = $requisicaoapi -> getBkoSes();
+	$_SESSION["phpsessid"]=$phpsessid;
+	echo '<script>var phpsessid="'.$phpsessid.'"</script>';
+	$idBko = $requisicaoapi -> num_responsavel($phpsessid);
+	$result = $requisicaoapi -> consulta_bko($phpsessid,1,$idBko);
+	include_once('tabela.php');
+}
+else{
+	unset($_SESSION["usuario"]);	
+	header("Location: index.php");
+}
+/*
+include_once('model/classDB.php');
+
+
+$conecta = new db();
+$senha= 'migra201125';
+$usuario = 'luis.silva';
+
+$requisicaoapi = new api($usuario, $senha);
+
+$tabela = 'followup';
+$idchamado = '207329';
+$pagina ='interacoes';
+$cron = date("Y-m-d H:i");
+$numInteracao = $requisicaoapi->consulta_ws($idchamado, $pagina)->count;
+
+var_dump($numInteracao);
+
+$dados_follow = array(
+      'idChamado' => $idchamado,
+      'numInteracao' => $numInteracao,
+      'usuario' => $usuario,
+      'executaCron' => $cron,
+);
+//inserir no banco de dados
+//$insert = $conecta->inserir($tabela, $dados_follow);
+
+//select do para comparar se houve alteração no chamado
+/*$select = $conecta->select($idchamado);
+$countws = implode("", $select);
+
+if($countws == $numInteracao){
+	echo "<br>";
+	echo "Os valores são iguais, manter para o follow";
+
+} else if($countws >= $numInteracao){
+	$conecta->delete($idchamado);
+	echo "removido do banco";
+
+} else {
+	echo "<br>";
+	echo "Opss... erro inesperado, fale com o dev do sistema";
+}
+
+
+//verificar se já passou 24 horas.
+$hora_banco = $conecta->select_hora($idchamado);
+$h = implode("", $hora_banco);
+echo $h;
+echo $cron;
+$timediff = strtotime($cron) - strtotime($h);
+
+echo $timediff;
+
+if($timediff > 86400){ 
+	echo "<br>";
+    echo 'Mais de 24 horas';
+} else {
+	echo "<br>";
+ 	echo 'Menos de 24 horas';
+}
+
+*/
--- index.php
+++ index.php
@@ -2,95 +2,33 @@
 session_start();
 include_once('model/classAPI.php');
 
-if(isset($_POST["usuario"])||isset($_SESSION["usuario"])){
-	$usuario = isset($_POST["usuario"]) ? $_POST["usuario"] : $_SESSION["usuario"];
-	$senha = isset($_POST["senha"]) ? $_POST["senha"] : $_SESSION["senha"];
+if(isset($_SESSION["usuario"])){
+	header("Location: admin.php");
+}
+else{
+	echo '
+			<form action="index.php" method="post">
+	 		<p>Usuário: <input type="text" name="usuario" /></p>
+	 		<p>Senha: <input type="password" name="senha" /></p>
+	 		<p><input value="Logar" type="submit" /></p>
+			</form>
+		';
+}
+if(isset($_POST["usuario"])&&isset($_POST["senha"])){
+	$usuario = $_POST["usuario"];
+	$senha = $_POST["senha"];
 
 	$requisicaoapi = new api($usuario, $senha);
 	if($requisicaoapi -> loga()){
 		$_SESSION["usuario"]=$usuario;
 		$_SESSION["senha"]=$senha;
-		$phpsessid = $requisicaoapi -> getBkoSes();
-		echo '<script>var phpsessid="'.$phpsessid.'"</script>';
-		$idBko = $requisicaoapi -> num_responsavel($phpsessid);
-		$result = $requisicaoapi -> consulta_bko($phpsessid,1,$idBko);
-		include_once('tabela.php');
+		echo "Conectado! Logando ...";
+		header("Location: admin.php");
 	}
 	else{
-		session_destroy();
-		echo "<script>alert('Dados inválidos');window.history.back();</script>";
+	echo "Dados inválidos";
+	unset($_SESSION["usuario"]);
+	unset($_SESSION["senha"]);
+	unset($_SESSION["phpsessid"]);
 	}
 }
-else{
-	echo '
-		<form action="index.php" method="post">
- 		<p>Usuário: <input type="text" name="usuario" /></p>
- 		<p>Senha: <input type="password" name="senha" /></p>
- 		<p><input value="Logar" type="submit" /></p>
-		</form>
-	';
-}
-
-/*
-include_once('model/classDB.php');
-
-
-$conecta = new db();
-$senha= 'migra201125';
-$usuario = 'luis.silva';
-
-$requisicaoapi = new api($usuario, $senha);
-
-$tabela = 'followup';
-$idchamado = '207329';
-$pagina ='interacoes';
-$cron = date("Y-m-d H:i");
-$numInteracao = $requisicaoapi->consulta_ws($idchamado, $pagina)->count;
-
-var_dump($numInteracao);
-
-$dados_follow = array(
-      'idChamado' => $idchamado,
-      'numInteracao' => $numInteracao,
-      'usuario' => $usuario,
-      'executaCron' => $cron,
-);
-//inserir no banco de dados
-//$insert = $conecta->inserir($tabela, $dados_follow);
-
-//select do para comparar se houve alteração no chamado
-/*$select = $conecta->select($idchamado);
-$countws = implode("", $select);
-
-if($countws == $numInteracao){
-	echo "<br>";
-	echo "Os valores são iguais, manter para o follow";
-
-} else if($countws >= $numInteracao){
-	$conecta->delete($idchamado);
-	echo "removido do banco";
-
-} else {
-	echo "<br>";
-	echo "Opss... erro inesperado, fale com o dev do sistema";
-}
-
-
-//verificar se já passou 24 horas.
-$hora_banco = $conecta->select_hora($idchamado);
-$h = implode("", $hora_banco);
-echo $h;
-echo $cron;
-$timediff = strtotime($cron) - strtotime($h);
-
-echo $timediff;
-
-if($timediff > 86400){ 
-	echo "<br>";
-    echo 'Mais de 24 horas';
-} else {
-	echo "<br>";
- 	echo 'Menos de 24 horas';
-}
-
-*/
