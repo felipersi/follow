400d0b0856fd7a8ecfc1e89b92e01c0c82ec9490
Bruno Minossi <b.minossi@gmail.com>
2019-09-28 04:37:38 -0300

    Interface de login corrigida

--- index.php
+++ index.php
@@ -7,11 +7,50 @@ if(isset($_SESSION["usuario"])){
 }
 else{
 	echo '
-			<form action="index.php" method="post">
-	 		<p>Usuário: <input type="text" name="usuario" /></p>
-	 		<p>Senha: <input type="password" name="senha" /></p>
-	 		<p><input value="Logar" type="submit" /></p>
-			</form>
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>BackOffice KingHost</title><!-- bootstrap -->
+	<link href="./css/bootstrap.css" rel="stylesheet">
+	<link href="./css/bootstrap-responsive.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<style type="text/css" title="currentStyle">
+
+		@import "./css/jquery-ui-1.8.10.custom.css";
+	    @import "./css/bo-admin.css";
+	    @import "./css/jquery.fancybox-1.3.4.css";
+	    @import "./css/tipTip.css";
+	</style>
+
+	</head>
+	<body>
+			<div id="telaLogin">
+    <span class="login-title">Login</span>
+    <h3 class="login-local">Backoffice</h3>
+    <form action="index.php" method="post">
+	    <div class="control-group">
+	    <div class="controls">
+	    <div class="input-icon left">
+	    <input type="text" required="required" class="m-wrap placeholder-no-fix valid" placeholder="Login" name="usuario" id="nome" />
+	    </div>
+	    </div>
+	    </div>
+
+	    <div class="control-group">
+	    <div class="controls">
+	    <div class="input-icon left">
+	    <input type="password" required="required" class="m-wrap placeholder-no-fix" placeholder="Senha" name="senha" id="senha" />
+	    </div>
+	    </div>
+	    </div>
+	    <div class="form-actions">
+	    <input name="submit" type="submit" class="btn btn-primary" value="Entrar" />        
+	    </div>
+
+	    <div class="message-box">
+	    </div>
+    </form>
+	</div>
 		';
 }
 if(isset($_POST["usuario"])&&isset($_POST["senha"])){
